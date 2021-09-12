f7a44fb258bd68d4f7756a7ed83514bd9e204886
pramodkumar <pramodkumar10.1991@gmail.com>
2021-09-12 16:10:44 +0530

    third commit

--- src/main/java/com/demo/tmforum/TmForumController.java
+++ src/main/java/com/demo/tmforum/TmForumController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TmForumController {
 	
-@GetMapping("/homepage")
+@GetMapping("/homepage1")
 public ResponseEntity<Object> display()
 {
 	
